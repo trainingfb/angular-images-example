5a6f6ed1afaaba9ffdeac6efb1009d41420d5baa
Fabio Biondi <info@fabiobiondi.com>
2024-01-16 00:00:13 +0100

    add CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
     <div class="page">
       <h1>Demo CDN + ngSRC</h1>
       
-      <img src="assets/emote-pad.png" alt="" width="400" height="400">
+      <img src="https://28v1knas.dev.cdn.imgeng.in/assets/emote-pad.png" alt="" width="400" height="400">
 
       <div>Powered by</div>
       <img
-        src="assets/cdn-logo.png" alt=""
+        src="https://28v1knas.dev.cdn.imgeng.in/assets/cdn-logo.png" alt=""
         [width]="200"
         [height]="70"
       >
